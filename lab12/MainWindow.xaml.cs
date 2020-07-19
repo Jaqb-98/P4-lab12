9badc6c729f60e02a28d3535d04d738ac65f0c74
Jakub Abramczyk <kubowianin@gmail.com>
2020-07-19 11:07:13 +0200

    lab12

---
+++ lab12/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace lab12
+{
+
+    public partial class MainWindow : Window
+    {
+        public TextBox TextBox { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBox = new TextBox();
+            this.DataContext = TextBox;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["Brush"] = new SolidColorBrush(Colors.Red);
+     
+        }
+    
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            string fileName = Environment.CurrentDirectory + @"\GreenBrush.xaml";
+            if (File.Exists(fileName))
+            {
+                using (FileStream fs = new FileStream(fileName, FileMode.Open))
+                {
+                    ResourceDictionary dic = (ResourceDictionary)XamlReader.Load(fs);
+                    Resources.MergedDictionaries.Add(dic);
+                }
+            }
+        }
+
+        private void Zoom(object sender, RoutedEventArgs e)
+        {
+            TextBox.Length += 10;
+        }
+    }
+}
---
+++ lab12/TextBox.cs
@@ -0,0 +1,28 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace lab12
+{
+    public class TextBox : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int _length = 100;
+
+        public int Length
+        {
+            get { return _length; }
+            set { 
+                _length = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
